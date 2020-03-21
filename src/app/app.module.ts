45e9c6b23327fa51ca89eff095c9f220ae8d7722
Telega <telega815@gmail.com>
2020-03-21 13:52:32 +0300

    -init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: WelcomePageComponent,
+    pathMatch: 'full'
+  // },
+  // {
+  //   path: 'service/signin',
+  //   component: SignInPageComponent
+  // },
+  // {
+  //   path: 'service/registration',
+  //   component: RegistrationPageComponent,
+  // },
+  // {
+  //   path: ':user',
+  //   component: FeedPageComponent,
+  // },
+  // {
+  //   path: '**',
+  //   component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
